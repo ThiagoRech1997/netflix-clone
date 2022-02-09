64876941c0f084ff3e5c5c5de633d2f621225ff9
Thiago Rech <thiagorech.1997@gmail.com>
2022-02-09 14:31:12 -0300

    Data Gift

---
+++ src/components/FeaturedMovie/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./style.css"
+
+export default ({item}) => {
+    return (
+        <section className="featured">
+            <div>
+                
+            </div>
+        </section>
+    )
+}

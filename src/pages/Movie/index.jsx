30f566aab9053d354c640a724e9c52a34f976acd
Thiago Rech <thiagorech.1997@gmail.com>
2022-03-01 19:44:43 -0300

    torrent it's work

--- src/pages/Movie/index.jsx
+++ src/pages/Movie/index.jsx
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
+import Form from 'react-bootstrap/Form'
+import { Link } from "react-router-dom"
+
 import tmdb from "./../../services/tmdb"
+import torrentApi from "./../../services/torrentApi"
 
 import "./style.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 export default function Movie(){
     const { id, midia } = useParams()
     const [movieList, setMovieList] = useState(null)
     const [midiaType, setMidiaType] = useState("tv")
+    const [options, setOptions] = useState([]);
+    const [torrent, setTorrent] = useState("undefined");
+    const [magnetLink, setMagnetLink] = useState({magnet: ""})
     let genres = []
+    let torrents = []
     
     useEffect(async()=>{
         let info = []
@@ -20,16 +29,53 @@ export default function Movie(){
             info = await tmdb.getMovieInfo(id, "movie")
             setMidiaType("movie")
         }
-
+        handleTorrentList()
         setMovieList(info)
-        console.log(movieList)
         await handleSetGenresList()
     }, [!movieList])
+
+    useEffect(async () => {
+        torrentApi.get("stop")
+    }, [])
+
 
     function handleSetGenresList(){
         for(let i in movieList.genres){
             genres.push(movieList.genres[i].name)
         }
+    }
+
+    async function handleTorrentList(){
+        if(midiaType === "tv"){
+            let torrent = await torrentApi.get(`getTorrent?title=${movieList.original_name}&date=${movieList.first_air_date}`)
+            torrents = torrent.data.torrents
+        }
+        if(midiaType === "movie"){
+            let torrent = await torrentApi.get(`getTorrent?title=${movieList.original_title}&date=${movieList.release_date}`)
+            torrents = torrent.data.torrents
+        }
+        console.log(torrents)
+
+        if (torrents.length > 0) {
+            const data = await torrents.map((item, index) => ({
+              key: index,
+              title: item.size + " " + item.seeds + " " + item.title,
+              value: item.link,
+            }));
+            await setOptions(data);
+            await setTorrent(data[0].title)
+            await setMagnetLink({magnet: data[0].value})
+        }
+    }
+    async function onPlay(){
+        for(let i in options){
+            if(torrent === options[i].title){
+                setMagnetLink({magnet: options[i].value})
+            }
+        }
+        console.log(torrent)
+        console.log(magnetLink)
+        torrentApi.get(`start?magnet=${magnetLink.magnet}`)
     }
 
     return(
@@ -54,6 +100,21 @@ export default function Movie(){
                                     <strong>{genres.join(', ')}</strong>
                                 </div>
                                 <div className="movie--description">{movieList.overview}</div>
+                                <div className="movie--selector">
+                                    <Form.Select
+                                        value={torrent}
+                                        onChange={e => setTorrent(e.target.value)}
+                                    >
+                                        {options.flatMap((data, index) => (
+                                            <option key={index}>{data.title}</option>
+                                        ))}
+                                    </Form.Select>
+                                    <div className="movie--buttons">
+                                        <Link to="/watch/play">
+                                            <a className="movie--play" onClick={() => onPlay()}>Play</a>
+                                        </Link>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -84,7 +145,7 @@ export default function Movie(){
                         </div>
                     </div>
                 </div>
-            }   
+            }
         </div>
     )
 }
